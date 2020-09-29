697ddf4b3cf2b3eba0bc86ff26ec6bc293104778
DeepaKaranam <dpakaranam@yahoo.com>
2020-09-29 16:57:49 +0530

    Updates

---
+++ precache-manifest.c1a91fede1c53271188a3e71dfc02753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1816d282a068603f1b52aabd7bc97f7",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "9d7917c05c09496a4760",
+    "url": "/Monsters-Rolodex/static/css/main.69c83242.chunk.css"
+  },
+  {
+    "revision": "df0547ebdd45e2628ad6",
+    "url": "/Monsters-Rolodex/static/js/2.95bbe305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.95bbe305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d7917c05c09496a4760",
+    "url": "/Monsters-Rolodex/static/js/main.b6e641df.chunk.js"
+  },
+  {
+    "revision": "78536e51920ede0aa52c",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.391f2a9e.js"
+  }
+]);
